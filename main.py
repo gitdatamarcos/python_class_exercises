cfdb07f18a651ba737b1d452a7721899e52b7d78
Marcos Antonio <143044598+gitdatamarcos@users.noreply.github.com>
2024-02-21 19:28:03 -0300

    Update main.py
    correcao da  linha 5

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 idade = int(input("Digite a sua idade: "))
 
 # Verificar o tipo de voto de acordo com a idade
-if idade < 15:
+if idade < 16:
   print("NÃO PODE VOTAR")
 elif idade in (16, 17) or idade > 65:
   print("VOTO OPCIONAL")
